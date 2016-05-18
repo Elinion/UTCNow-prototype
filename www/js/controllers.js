86fd56cd0c8ad079408b718d71601c2c7104c212
alexandre abrantes <alexandre.abts@gmail.com>
2016-05-18 15:57:41 +0200

    interface base (done by Theo)

---
+++ www/js/controllers.js
@@ -0,0 +1,38 @@
+angular.module('app.controllers', [])
+  
+.controller('toutLUTCCtrl', function($scope) {
+
+})
+   
+.controller('monAgendaCtrl', function($scope) {
+
+})
+   
+.controller('agendasFavorisCtrl', function($scope) {
+
+})
+         
+.controller('bienvenueCtrl', function($scope) {
+
+})
+   
+.controller('paramTresCtrl', function($scope) {
+
+})
+   
+.controller('ProposCtrl', function($scope) {
+
+})
+   
+.controller('defaultAgendaCtrl', function($scope) {
+
+})
+   
+.controller('carteVNementCtrl', function($scope) {
+
+})
+   
+.controller('editerUnVNementCtrl', function($scope) {
+
+})
+ 
